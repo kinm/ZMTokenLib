5030f0af8b36f5327121650271c12db051873acd
kinm <2450346849@qq.com>
2024-04-29 17:04:43 +0800

    init

---
+++ src/tokens.js
@@ -0,0 +1,7 @@
+// 获取coingecko热门代币列表并与tokens目录中的代币对比
+
+
+// 将tokens目录中不存在的代币添加到tokens目录中
+// 代币信息包括：name, symbol, contractAddress, chainId, decimals, logoURI, website
+// 代币信息来源：coingecko
+// 代币信息存储格式：JSON
